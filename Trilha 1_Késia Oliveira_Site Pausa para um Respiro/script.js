304c819c376c75c14cfd13e060905f96b7b14c72
Kesia Oliveira <kesiaoliveira@msn.com>
2024-07-07 11:52:15 -0300

    commit

---
+++ Trilha 1_Késia Oliveira_Site Pausa para um Respiro/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('nav a').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
